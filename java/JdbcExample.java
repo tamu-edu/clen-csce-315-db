283ec50023e0b1c00d86bfea55ce27029701fa92
Connor White <69062901+CorniiDog@users.noreply.github.com>
2025-02-19 09:27:42 -0600

    Update JdbcExample.java
    Uses command stmt.executeUpdate(sql); instead of stmt.execute(sql); as execute is not a valid command for certain versions.

--- java/JdbcExample.java
+++ java/JdbcExample.java
@@ -45,7 +45,8 @@ public class JdbcExample {
       sql = "USE adventureworks;";
       ResultSet rs;
       //rs = stmt.executeQuery(sql); only works with old versions
-      rs = stmt.execute(sql);
+      //rs = stmt.execute(sql); 'execute' may or may not be a valid command depending on version
+      stmt.executeUpdate(sql);
       sql = "SELECT EmployeeId, LoginID from employee";
       rs = stmt.executeQuery(sql);
 
